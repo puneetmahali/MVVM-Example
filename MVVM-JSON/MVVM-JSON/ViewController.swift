d8427ac461ae4608f56ef16121037c4aea669fce
Puneet Mahali <iospuneet@gmail.com>
2020-04-15 20:19:37 +0200

    Master- Project Setup

---
+++ MVVM-JSON/MVVM-JSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-JSON
+//
+//  Created by Puneet Personal on 15.04.20.
+//  Copyright © 2020 Puneet Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
