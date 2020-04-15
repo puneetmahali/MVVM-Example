16443d278ca7b1be22e0ae6645da8a8681422124
Puneet Mahali <iospuneet@gmail.com>
2020-04-15 21:11:23 +0200

    dev- Calling the API using URLSession.

--- MVVM-JSON/MVVM-JSON/Controller/ViewController.swift
+++ MVVM-JSON/MVVM-JSON/Controller/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var viewModelUser = UserViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        viewModelUser.getAllUsersData()
     }
 
 
---
+++ MVVM-JSON/MVVM-JSON/ViewModel/UserViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  UserViewModel.swift
+//  MVVM-JSON
+//
+//  Created by Puneet Personal on 15.04.20.
+//  Copyright © 2020 Puneet Personal. All rights reserved.
+//
+
+import UIKit
+
+class UserViewModel {
+    var usersArray = [UserModel]()
+    weak var vc : ViewController?
+    
+    let mainURL = "https://jsonplaceholder.typicode.com/todos/"
+    
+    //MARK: Using URLSession
+    func getAllUsersData() {
+        URLSession.shared.dataTask(with: URL(string: mainURL)!) { (data, response, error)
+            in
+            if error == nil {
+                if let data = data {
+                    do {
+                        let userResponse = try JSONDecoder().decode([UserModel].self, from: data)
+                        print(userResponse)
+                    }catch let err {
+                        print(err.localizedDescription)
+                    }
+                }
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+            
+        }.resume()
+    }
+    
+}
